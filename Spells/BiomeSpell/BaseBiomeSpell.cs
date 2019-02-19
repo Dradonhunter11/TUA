e9a0777ccc3e96c4df8ddd96e0a5c63ca4552f79
Dradonhunter11 <dradonhunter11@gmail.com>
2019-02-19 17:30:53 -0500

    Biome Spell Initial - Legacy feature coming back

---
+++ Spells/BiomeSpell/BaseBiomeSpell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using TUA.API;
+
+namespace TUA.Spells.BiomeSpell
+{
+    abstract class BaseBiomeSpell : TUAModLegacyItem, ISpell
+    {
+        public abstract bool Cast(Player player);
+
+        public virtual bool GetColor(out Color color)
+        {
+            color = default(Color);
+            return false;
+        }
+
+        public sealed override bool UseItem(Player player) => Cast(player);
+    }
+}
---
+++ Spells/BiomeSpell/DesertSpell.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using TUA.API;
+
+namespace TUA.Spells.BiomeSpell
+{
+    internal class DesertSpell : BaseBiomeSpell
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Desert Desolation");
+            Tooltip.SetDefault("A spell created by the humans a long time ago.");
+            Tooltip.AddLine("The legends say that any land touching it get scorched");
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            item.shoot = mod.ProjectileType("DesertSpellProjectile");
+        }
+
+        public override bool Cast(Player player)
+        {
+            return false;
+        }
+
+        public override bool GetColor(out Color color)
+        {
+            color = new Color(237, 201, 175);
+            return true;
+        }
+    }
+
+    internal class DesertSpellProjectile : BaseBiomeSpellProjectile
+    {
+        public override void Convert(int x, int y)
+        {
+            Tile tile = Main.tile[x, y];
+            if (tile.wall == WallID.Dirt || WallID.Sets.Corrupt[tile.wall] || WallID.Sets.Crimson[tile.wall])
+            {
+                TileSpreadUtils.ChangeWall(x, y, WallID.Sandstone);
+            }
+
+            if (tile.type == TileID.Dirt || TileID.Sets.Conversion.Grass[tile.type] || tile.type == TileID.SnowBlock)
+            {
+                TileSpreadUtils.ChangeTile(x, y, TileID.Sand);
+            }
+
+            if (TileID.Sets.Conversion.Stone[tile.type])
+            {
+                TileSpreadUtils.ChangeTile(x, y, TileID.Sandstone);
+            }
+
+            if (TileID.Sets.Conversion.Ice[tile.type])
+            {
+                TileSpreadUtils.ChangeTile(x, y, TileID.HardenedSand);
+            }
+
+        }
+    }
+}
