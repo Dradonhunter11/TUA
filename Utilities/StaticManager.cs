816077fb0bf174c1065f106026822b56c01058ef
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2019-03-18 18:56:43 -0400

    Created Static Universal Manager Class

---
+++ Utilities/StaticManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUA.Utilities
+{
+	public static class StaticManager<T>
+	{
+		private static readonly Dictionary<string, T> ITEMS;
+		private static Dictionary<string, string> ITEM_ID_TO_NAME_MAP;
+
+		private static readonly T DEFAULT;
+
+		public static int Count => ITEMS.Count;
+
+		static StaticManager()
+		{
+			ITEMS = new Dictionary<string, T>();
+			ITEM_ID_TO_NAME_MAP = new Dictionary<string, string>();
+			DEFAULT = default(T);
+		}
+
+		public static void AddItem(string idname, T item)
+		{
+			AddItem(idname, idname, item);
+		}
+
+		public static void AddItem(string id, string name, T item)
+		{
+			if (ContainsItem(id))
+				return;
+
+			ITEM_ID_TO_NAME_MAP.Add(id, name);
+			ITEMS.Add(id, item);
+		}
+
+		public static void RemoveItem(string id)
+		{
+			if (ContainsItem(id))
+			{
+				ITEMS.Remove(id);
+				ITEM_ID_TO_NAME_MAP.Remove(id);
+			}
+		}
+
+		public static bool ContainsItem(string id)
+		{
+			return ITEMS.ContainsKey(id) && ITEM_ID_TO_NAME_MAP.ContainsKey(id);
+		}
+
+		public static string GetItemID(int index)
+		{
+			return ITEMS.Keys.ToArray()[index];
+		}
+
+		public static string GetItemName(string id)
+		{
+			return ContainsItem(id) ? ITEM_ID_TO_NAME_MAP[id] : null;
+		}
+
+		public static string GetItemName(int index)
+		{
+			return GetItemName(GetItemID(index));
+		}
+
+		public static T GetItem(string id)
+		{
+			return ContainsItem(id) ? ITEMS[id] : DEFAULT;
+		}
+
+		public static T GetItem(int index)
+		{
+			return GetItem(GetItemID(index));
+		}
+	}
+}
