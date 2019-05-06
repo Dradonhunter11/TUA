ad5147de61626fdd4d270646e0e57a42c6f57c5e
Agrair <firedrakehugo@gmail.com>
2019-05-05 19:42:46 -0700

    More watcher

--- NPCs/NewBiome/Wasteland/WastelandWatcher.cs
+++ NPCs/NewBiome/Wasteland/WastelandWatcher.cs
@@ -2,12 +2,17 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace TUA.NPCs.NewBiome.Wasteland
 {
     class WastelandWatcher : ModNPC
     {
+        private const byte AI_TileI = 0;
+        private const byte AI_TileJ = 1;
+        private const byte AI_State = 2;
+
         public static void Spawn(int x, int y)
         {
             bool success = false;
@@ -38,16 +43,28 @@ namespace TUA.NPCs.NewBiome.Wasteland
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Wasteland Watcher");
+            Main.npcFrameCount[npc.type] = 1;
         }
 
         public override void AI()
         {
-            
+            // TODO
         }
 
         public override void SetDefaults()
         {
-            base.SetDefaults();
+            npc.height = 38;
+            npc.width = 38;
+            npc.value = 100000;
+            npc.HitSound = SoundID.NPCHit41;
+            npc.DeathSound = SoundID.NPCDeath44;
+            npc.buffImmune[20] = true;
+            npc.buffImmune[24] = true;
+            npc.aiStyle = -1;
+            npc.lifeMax = 120;
+            npc.defense = 18;
+            npc.knockBackResist = 0;
+            npc.noGravity = true;
         }
 
         float rotation = -1;
@@ -55,7 +72,7 @@ namespace TUA.NPCs.NewBiome.Wasteland
         {
             if (rotation == -1)
             {
-                rotation = (npc.Center - new Vector2(npc.ai[0], npc.ai[1])).ToRotation();
+                rotation = (npc.Center - new Vector2(npc.ai[AI_TileI], npc.ai[AI_TileJ])).ToRotation();
             }
             spriteBatch.Draw(Main.npcTexture[npc.type],
                 npc.Center, 
